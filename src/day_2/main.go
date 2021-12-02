619025c5ac902bfd5d4671d430eadc96394d9a59
Sergey Larionov <sergey.larionov@jetbrains.com>
2021-12-02 09:36:28 +0100

    Solve second part of day 2

--- src/day_2/main.go
+++ src/day_2/main.go
@@ -9,6 +9,11 @@ import (
 	"strings"
 )
 
+func main() {
+	PartOne()
+	PartTwo()
+}
+
 func readSourceFile() []string {
 	var text []string
 
@@ -29,7 +34,33 @@ func readSourceFile() []string {
 	return text
 }
 
-func main() {
+func PartTwo() {
+	forward, aim, depth := 0, 0, 0
+	source := readSourceFile()
+
+	for _, v := range source {
+		command := strings.Split(v, " ")
+		switch command[0] {
+		case "forward":
+			p := GetValue(command[1])
+			forward += p
+
+			if aim != 0 {
+				depth = depth + (aim * p)
+			}
+		case "down":
+			p := GetValue(command[1])
+			aim += p
+		case "up":
+			p := GetValue(command[1])
+			aim -= p
+		}
+	}
+
+	fmt.Printf("Result of second path is %v \n", forward*depth)
+}
+
+func PartOne() {
 	depth, forward := 0, 0
 	source := readSourceFile()
 
@@ -49,7 +80,7 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Result is %d", depth*forward)
+	fmt.Printf("Result of the first part is %d \n", depth*forward)
 
 }
 
